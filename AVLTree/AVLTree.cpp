8d12e44da21c5e9381f3e761268e95a9f76bbaa3
Ivan Tarasov <ivan.tarasov12345@gmail.com>
2022-11-17 13:38:34 +0500

    Добавьте файлы проекта.

---
+++ AVLTree/AVLTree.cpp
@@ -0,0 +1,43 @@
+﻿// AVLTree.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "AVLTree.h"
+using namespace Tree;
+
+template<typename T>
+void AVLTree<T>::add(T data)
+{
+
+}
+
+template<typename T>
+void AVLTree<T>::remove(T data)
+{
+
+}
+
+template<typename T>
+bool AVLTree<T>::search(T data)
+{
+
+}
+
+template<typename T>
+T* AVLTree<T>::toArray()
+{
+
+}
+
+template<typename T>
+void AVLTree<T>::restore()
+{
+
+}
+
+template<typename T>
+void AVLTree<T>::clear()
+{
+
+}
---
+++ AVLTree/AVLTree.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+namespace Tree
+{
+	template<typename T>
+	class AVLTree
+	{
+	public:
+		AVLTree() { root = 0; }
+		AVLTree(T data) { root = new Node(data); }
+		~AVLTree() { clear(); }
+
+		// добавление
+		void add(T data);
+
+		// удаление
+		void remove(T data);
+
+		// поиск
+		bool search(T data);
+
+		// представление в виде массива 
+		T* toArray();
+
+	private:
+		struct Node
+		{
+			T data;
+			Node* left;
+			Node* right;
+
+			Node(T data)
+			{
+				this->data = data;
+				left = 0;
+				right = 0;
+			}
+		};
+
+		// корень
+		Node* root;
+
+		// восстановление свойств
+		void restore();
+
+		// удаление дерева
+		void clear();
+	};
+}
